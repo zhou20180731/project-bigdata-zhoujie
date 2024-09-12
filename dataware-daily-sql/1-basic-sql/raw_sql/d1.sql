5d2a1b4250c7dae02b9a1ccd43b31c9194a3bbfc
zhoujie <2396225572@qq.com>
2024-09-12 08:24:47 +0800

    最后提交

--- dataware-daily-sql/1-basic-sql/raw_sql/d1.sql
+++ dataware-daily-sql/1-basic-sql/raw_sql/d1.sql
@@ -18,30 +18,43 @@ USE hive_sql_zg5 ;
 
 -- todo: 1.1、简单查询
 -- 1）、检索课程编号为“04”且分数小于60的学生的课程信息，结果按分数降序排列；
-select  stu_id
-     , course_id
-     , score
-from hive_sql_zg5.score_info
+SELECT
+      stu_id
+      , course_id
+      , score
+FROM hive_sql_zg5.score_info
 WHERE course_id='04' AND score<60
 ORDER BY  score DESC ;
 
 -- 2）、查询数学成绩不及格的学生和其对应的成绩，按照学号升序排序；
+-- 2.1 查询数学成绩的编号
 SELECT course_id
 FROM hive_sql_zg5.course_info
 WHERE course_name ='数学'
 ;
 
+--2.2
+SELECT * FROM hive_sql_zg5. score_info
+WHERE  course_id=(SELECT course_id
+                  FROM hive_sql_zg5.course_info
+                  WHERE course_name ='数学')
+
+  AND  score<60
+ORDER BY stu_id desc ;
+
 -- 3）、查询姓名中带“冰”的学生名单；
-select stu_id
+SELECT stu_id
      , stu_name
      , birthday
      , sex
     from student_info
     WHERE  stu_name LIKE '%冰%';
 -- 4）、查询姓“王”老师的个数；
-select  count()
-From teacher_info
-where tea_name like  '%王%';
+
+select  count(tea_id) as cnt
+From hive_sql_zg5.teacher_info
+where tea_name like  '王%';
+
 
 -- todo：1.2、汇总分析
 /*
@@ -53,7 +66,25 @@ where tea_name like  '%王%';
 		min 最小
 */
 -- 1）、查询编号为“02”的课程的总成绩；
+-- SELECT
+--     '02' AS score_name
+-- from  hive_sql_zg5.score_info
+-- Where course_id ='02';
 
+
+SELECT
+    '02' AS score_name
+     , sum(score) AS sum_score
+     , count(stu_id) AS cnt_stu
+     , max(score) AS max_score
+     , min(score) AS min_score
+     , round(avg(score), 2) AS avg_score
+     , cast(avg(score) AS DECIMAL(16, 2)) AS avg_score
+     , avg(score) AS avg_score
+     , collect_list(score) AS score_list
+FROM hive_sql_zg5.score_info
+WHERE course_id = '02'
+;
 
 
 -- 2）、查询参加考试的学生个数；
@@ -63,9 +94,23 @@ where tea_name like  '%王%';
     案例剖析：https://blog.51cto.com/u_16099335/6687050
 */
 
+SELECT
+    count(DISTINCT stu_id) AS stu_cnt
+FROM hive_sql_zg5.score_info
+;
 
 
+SELECT
+    count(stu_id) AS stu_cnt
+FROM (
+--           group by 去重
+         SELECT
+             stu_id
+         FROM hive_sql_zg5.score_info
+         GROUP BY stu_id
+     ) t1
 
+;
 /**
 -- 查询日志
 [bwie@node101 ~]$ hv.sh beeline
